3d71a8747111a75e20e3a91d415a27b643cab7e1
johax aldertwig <carl.johan.ahlqvist@outlook.com>
2024-04-04 10:24:10 +0200

    Frontend årtal och lite backend. Static SkatteAPI hämtar data bara en gång.

--- KommunSkatter/Data/SkatteAPI.cs
+++ KommunSkatter/Data/SkatteAPI.cs
@@ -6,27 +6,43 @@ namespace KommunSkatter.Data
 {
     public class SkatteAPI
     {
+        private static List<Kommun> _Kommuner = null;
+
         public static async Task<List<Kommun>> GetAPIdataAsync()
         {
-            List<Kommun> kommuner = new List<Kommun>();
-
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://skatteverket.entryscape.net/");
-            client.DefaultRequestHeaders.Clear();
-
-            HttpResponseMessage response = await client.GetAsync("rowstore/dataset/c67b320b-ffee-4876-b073-dd9236cd2a99");
-            if (response.IsSuccessStatusCode)
+            if (_Kommuner == null)
             {
-                var data = response.Content.ReadAsStringAsync().Result;
-                var json = JsonSerializer.Deserialize<Kommun>(data);
-                kommuner.Add(json);
-            }
+                 _Kommuner = new List<Kommun>();
+            } 
             else
             {
-                Console.WriteLine("Error");
-            }
+                if (_Kommuner.Count == 0)
+                {
 
-            return kommuner;
+                    HttpClient client = new HttpClient();
+                    client.BaseAddress = new Uri("https://skatteverket.entryscape.net/");
+                    client.DefaultRequestHeaders.Clear();
+                    HttpResponseMessage response = await client.GetAsync("rowstore/dataset/c67b320b-ffee-4876-b073-dd9236cd2a99");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var data = response.Content.ReadAsStringAsync().Result;
+                        var json = JsonSerializer.Deserialize<Kommun>(data);
+                        _Kommuner.Add(json);
+                        Console.WriteLine("Data hämtad");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Data redan hämtad");
+                }
+            }
+        
+            return _Kommuner;
         }
     }
 }
